e4fbf415e11feed362c0be5cde103a5fb40baa3e
VIGNESH007-DE <vigneshwaranethicalhacker@gmail.com>
2025-04-08 23:52:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "PASTE YOUR API KEY HERE...."; // Your API key here
+const API_KEY = "AIzaSyC2mD7JKNhHs9GOPAiXKKkaVNnnvLs-GBk"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
